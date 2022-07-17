b7d5c43e53fb758a3c5224e8493be2d4d1ca753b
Mikolly12 <ymayank516@gmail.com>
2022-07-17 18:38:49 +0530

    final

---
+++ app/src/main/java/com/example/bluetoothdesign/ForgetPage.kt
@@ -0,0 +1,11 @@
+package com.example.bluetoothdesign
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ForgetPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forget_page)
+    }
+}
